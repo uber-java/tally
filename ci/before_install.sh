1717b3b3f129e58092aeeb70ab1b774f35a89c46
Justin Chan <juchan@uber.com>
2017-09-07 10:46:43 -0400

    Use jar with encryption algorithm implementations

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -5,7 +5,7 @@ gpg --fast-import ci/signing.gpg
 
 # Workaround to using openjdk7 with Gradle due to security issue:
 # https://github.com/gradle/gradle/issues/2421
-BCPROV_FILENAME=bcprov-jdk15on-158.jar
+BCPROV_FILENAME=bcprov-ext-jdk15on-158.jar
 wget "https://bouncycastle.org/download/${BCPROV_FILENAME}"
 sudo mv $BCPROV_FILENAME /usr/lib/jvm/java-7-openjdk-amd64/jre/lib/ext
 echo "security.provider.1=org.bouncycastle.jce.provider.BouncyCastleProvider" | sudo tee -a /etc/java-7-openjdk/security/java.security

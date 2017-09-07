686bff48f192f48e299be4ad3da8d619b332b22d
Justin Chan <juchan@uber.com>
2017-09-07 10:42:07 -0400

    Use security workaround to use openjdk7

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
+# Unencrypt signing file
 openssl aes-256-cbc -K $encrypted_258d8124f158_key -iv $encrypted_258d8124f158_iv -in ci/signing.gpg.enc -out ci/signing.gpg -d
 gpg --fast-import ci/signing.gpg
+
+# Workaround to using openjdk7 with Gradle due to security issue:
+# https://github.com/gradle/gradle/issues/2421
+BCPROV_FILENAME=bcprov-jdk15on-158.jar
+wget "https://bouncycastle.org/download/${BCPROV_FILENAME}"
+sudo mv $BCPROV_FILENAME /usr/lib/jvm/java-7-openjdk-amd64/jre/lib/ext
+echo "security.provider.1=org.bouncycastle.jce.provider.BouncyCastleProvider" | sudo tee -a /etc/java-7-openjdk/security/java.security
+sudo perl -pi.bak -e 's/^(security\.provider\.)([0-9]+)/$1.($2+1)/ge' /etc/java-7-openjdk/security/java.security

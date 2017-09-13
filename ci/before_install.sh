8291cd1ae421dcefceeb6a2b284b74681ea03076
Justin Chan <juchan@uber.com>
2017-09-13 15:28:00 -0400

    Only compile thrift if explicitly set a property to; update DEVELOPER docs to reflect on this

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -10,13 +10,3 @@ wget "https://bouncycastle.org/download/${BCPROV_FILENAME}"
 sudo mv $BCPROV_FILENAME /usr/lib/jvm/java-7-openjdk-amd64/jre/lib/ext
 sudo perl -pi.bak -e 's/^(security\.provider\.)([0-9]+)/$1.($2+1)/ge' /etc/java-7-openjdk/security/java.security
 echo "security.provider.1=org.bouncycastle.jce.provider.BouncyCastleProvider" | sudo tee -a /etc/java-7-openjdk/security/java.security
-
-# Install Thrift 0.9.3
-sudo apt-get update -qq
-sudo apt-get install libboost-dev libboost-test-dev libboost-program-options-dev libevent-dev automake libtool flex bison pkg-config g++ libssl-dev
-pushd .
-cd
-wget http://www.us.apache.org/dist/thrift/0.9.3/thrift-0.9.3.tar.gz
-tar xfz thrift-0.9.3.tar.gz
-cd thrift-0.9.3 && ./configure && sudo make install
-popd

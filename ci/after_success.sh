d84b88050d2304b8226684c0014602ffa2a3fd79
Justin Chan <juchan@uber.com>
2017-11-06 20:00:55 -0500

    Have Travis upload artifacts and release to Maven Central

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -2,8 +2,9 @@
 # Generate coverage report
 ./gradlew jacocoRootReport coveralls
 
-# Publish Javadoc
+# Tasks to do only on an actual release
 if [ "$TRAVIS_JDK_VERSION" == "openjdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+    # Publish Javadoc
     git checkout --orphan gh-pages
     git reset
     cp -r build/docs/javadoc/ docs
@@ -11,4 +12,7 @@ if [ "$TRAVIS_JDK_VERSION" == "openjdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false
     git add -A
     git commit -m "Update Javadoc"
     git push -f https://${GITHUB_TOKEN}@github.com/uber-java/tally.git gh-pages
+
+    # Upload artifacts and release to Sonatype to be synced to Maven Central
+   ./gradlew uploadArchives closeAndReleaseRepository
 fi

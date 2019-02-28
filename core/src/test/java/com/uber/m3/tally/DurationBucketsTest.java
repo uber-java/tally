cf30fb0d109403b2c4497d94f6ef437c25b67550
Jigar Joshi <jmj@uber.com>
2019-02-28 13:15:42 -0800

    Added custom buckets for Value and Duration

--- core/src/test/java/com/uber/m3/tally/DurationBucketsTest.java
+++ core/src/test/java/com/uber/m3/tally/DurationBucketsTest.java
@@ -33,6 +33,7 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThat;
 
 public class DurationBucketsTest {
     @Test
--- core/src/test/java/com/uber/m3/tally/ValueBucketsTest.java
+++ core/src/test/java/com/uber/m3/tally/ValueBucketsTest.java
@@ -28,6 +28,7 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThat;
 
 public class ValueBucketsTest {
     @Test

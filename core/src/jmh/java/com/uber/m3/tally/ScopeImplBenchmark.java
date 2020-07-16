411b2b2c03c310a489cff7a65ca1c1e7ffbf8416
Alexey Kudinkin <alexey.kudinkin@uber.com>
2020-07-16 12:30:32 -0700

    Added phony benchmark

--- core/src/jmh/java/com/uber/m3/tally/ScopeImplBenchmark.java
+++ core/src/jmh/java/com/uber/m3/tally/ScopeImplBenchmark.java
@@ -116,4 +116,9 @@ public class ScopeImplBenchmark {
         }
 
     }
+
+    @Benchmark
+    public void scopeReportingBenchmark(BenchmarkState state) {
+        state.scope.reportLoopIteration();
+    }
 }

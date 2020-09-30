2c84fef42e8e619d09320d0b57f13f63d1723557
Justin Chan <juchan@uber.com>
2020-09-30 19:28:05 -0400

    Make lint happy

--- m3/src/test/java/com/uber/m3/tally/m3/SocketCloseTest.java
+++ m3/src/test/java/com/uber/m3/tally/m3/SocketCloseTest.java
@@ -1,6 +1,25 @@
+// Copyright (c) 2020 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package com.uber.m3.tally.m3;
 
-import com.uber.m3.tally.Counter;
 import com.uber.m3.tally.Timer;
 import com.uber.m3.tally.RootScopeBuilder;
 import com.uber.m3.tally.StatsReporter;
@@ -12,7 +31,6 @@ import org.junit.Test;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.net.SocketException;
@@ -31,7 +49,7 @@ public class SocketCloseTest {
     public void setup() {
         try {
             socket = new DatagramSocket(PORT);
-        } catch(SocketException e) {
+        } catch (SocketException e) {
             e.printStackTrace();
         }
         new UDPServer(socket).start();
@@ -62,7 +80,7 @@ public class SocketCloseTest {
             System.out.println("socket closed!");
 
             Thread.sleep(120_000);
-        } catch(InterruptedException e) {
+        } catch (InterruptedException e) {
             System.out.println("Interrupted");
         }
     }
@@ -101,7 +119,7 @@ public class SocketCloseTest {
 //                    packet = new DatagramPacket(buf, buf.length, address, port);
 //                    String received = new String(packet.getData(), 0, packet.getLength());
 //                    System.out.println(String.format("Received: %s", received));
-                } catch(IOException e) {
+                } catch (IOException e) {
                     break;
                 }
             }

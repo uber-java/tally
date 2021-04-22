5cb9f0f17cb9bb15b741b1a94a09b9359095c4b4
Quanzheng Long <qlong@indeed.com>
2021-04-22 11:41:31 -0700

    address comments for validCharacters

--- core/src/main/java/com/uber/m3/tally/sanitizers/CharRange.java
+++ core/src/main/java/com/uber/m3/tally/sanitizers/CharRange.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Uber Technologies, Inc.
+// Copyright (c) 2021 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -46,5 +46,9 @@ public class CharRange {
 
     char high() {
         return high;
+    }
+
+    public boolean isWithinRange(char ch){
+        return (ch >= low) && (ch <= high);
     }
 }
--- core/src/main/java/com/uber/m3/tally/sanitizers/ValidCharacters.java
+++ core/src/main/java/com/uber/m3/tally/sanitizers/ValidCharacters.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 Uber Technologies, Inc.
+// Copyright (c) 2021 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -22,7 +22,8 @@ package com.uber.m3.tally.sanitizers;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.Function;
 
 /**
@@ -38,73 +39,88 @@ public class ValidCharacters {
     /**
      * ALPHANUMERIC_RANGE is the range of alphanumeric characters.
      */
-    public static final List<CharRange> ALPHANUMERIC_RANGE =
-        Arrays.asList(
+    public static final Set<CharRange> ALPHANUMERIC_RANGE =
+            new HashSet<>(Arrays.asList(
             CharRange.of('a', 'z'),
             CharRange.of('A', 'Z'),
-            CharRange.of('0', '9'));
+            CharRange.of('0', '9')));
 
     /**
      * UNDERSCORE_CHARACTERS contains the underscore character.
      */
-    public static final List<Character> UNDERSCORE_CHARACTERS = Collections.singletonList('_');
+    public static final Set<Character> UNDERSCORE_CHARACTERS = Collections.singleton('_');
 
     /**
      * UNDERSCORE_DASH_CHARACTERS contains the underscore and dash characters.
      */
-    public static final List<Character> UNDERSCORE_DASH_CHARACTERS = Arrays.asList('_', '-');
+    public static final Set<Character> UNDERSCORE_DASH_CHARACTERS = new HashSet<>(Arrays.asList('_', '-'));
 
     /**
      * UNDERSCORE_DASH_DOT_CHARACTERS contains the underscore, dash and dot characters.
      */
-    public static final List<Character> UNDERSCORE_DASH_DOT_CHARACTERS = Arrays.asList('_', '-', '.');
+    public static final Set<Character> UNDERSCORE_DASH_DOT_CHARACTERS = new HashSet<>(Arrays.asList('_', '-', '.'));
 
-    private final List<CharRange> ranges;
-    private final List<Character> characters;
+    private final Set<CharRange> validRanges;
+    private final Set<Character> validCharacters;
 
-    private ValidCharacters(List<CharRange> ranges, List<Character> characters) {
-        this.ranges = ranges != null ? ranges : Collections.emptyList();
-        this.characters = characters != null ? characters : Collections.emptyList();
+    private ValidCharacters(Set<CharRange> validRanges, Set<Character> validCharacters) {
+        this.validRanges = (validRanges != null) ? validRanges : Collections.emptySet();
+        this.validCharacters = (validCharacters != null) ? validCharacters : Collections.emptySet();
     }
 
-    public static ValidCharacters of(List<CharRange> ranges, List<Character> characters) {
+    /**
+     * returns an instance of ValidCharacters
+     * @param ranges
+     * @param characters
+     * @return
+     */
+    public static ValidCharacters of(Set<CharRange> ranges, Set<Character> characters) {
         return new ValidCharacters(ranges, characters);
     }
 
-    Function<String, String> sanitizeStringFunc(char repChar) {
-        return value -> {
-            StringBuilder buffer = null;
+    /**
+     * returns if a char is valid
+     * a char is valid if it's within range of any range of validRanges  or within validCharacters
+     * @param ch
+     * @return
+     */
+    private boolean isValid(char ch){
+        return  validRanges.stream().anyMatch(range -> (range.isWithinRange(ch)))
+                || validCharacters.contains(ch);
+    }
 
-            for (int i = 0; i < value.length(); i++) {
-                char ch = value.charAt(i);
+    Function<String, String> sanitizeStringFunc(char replaceChar) {
+        return input -> {
+            StringBuilder output = null;
+
+            for (int i = 0; i < input.length(); i++) {
+                char currChar = input.charAt(i);
 
                 // first check if the provided character is valid
-                boolean validCurr =
-                    ranges.stream().anyMatch(range -> ch >= range.low() && ch <= range.high())
-                        || characters.stream().anyMatch(character -> character.equals(ch));
+                boolean isCurrValid = isValid(currChar);
 
                 // if it's valid, we can optimize allocations by avoiding copying
-                if (validCurr) {
-                    if (buffer != null) {
-                        buffer.append(ch);
+                if (isCurrValid) {
+                    if (output != null) {
+                        output.append(currChar);
                     }
                     continue;
                 }
 
                 // the character is invalid, and the buffer has not been initialized
                 // so we initialize the buffer and back-fill.
-                if (buffer == null) {
-                    buffer = new StringBuilder(value.length());
-                    buffer.append(value, 0, i);
+                if (output == null) {
+                    output = new StringBuilder(input.length());
+                    output.append(input, 0, i);
                 }
 
                 // write the replacement character
-                buffer.append(repChar);
+                output.append(replaceChar);
             }
 
             // return input un-touched if the buffer has not been initialized
             // otherwise, return the newly constructed buffer string
-            return buffer == null ? value : buffer.toString();
+            return (output == null) ? input : output.toString();
         };
     }
 }

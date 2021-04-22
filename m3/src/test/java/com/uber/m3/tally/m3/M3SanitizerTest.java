bb561ecf9f7c6f4a1b169ef5775e28ba679d3ae2
Quanzheng Long <qlong@indeed.com>
2021-04-22 11:59:55 -0700

    more fix for copyright

--- m3/src/test/java/com/uber/m3/tally/m3/M3SanitizerTest.java
+++ m3/src/test/java/com/uber/m3/tally/m3/M3SanitizerTest.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 Uber Technologies, Inc.
+// Copyright (c) 2021 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -23,7 +23,8 @@ package com.uber.m3.tally.m3;
 import com.uber.m3.tally.sanitizers.ScopeSanitizer;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class M3SanitizerTest {
 

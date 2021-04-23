315c5593915ccabcb57da2827ed0cbfef840b5b8
Quanzheng Long <qlong@indeed.com>
2021-04-23 11:57:15 -0700

    Bench with sanitizer
    add benchmark code

--- core/src/jmh/java/com/uber/m3/tally/ScopeImplBenchmark.java
+++ core/src/jmh/java/com/uber/m3/tally/ScopeImplBenchmark.java
@@ -20,6 +20,8 @@
 
 package com.uber.m3.tally;
 
+import com.uber.m3.tally.sanitizers.ScopeSanitizerBuilder;
+import com.uber.m3.tally.sanitizers.ValidCharacters;
 import com.uber.m3.util.Duration;
 import com.uber.m3.util.ImmutableMap;
 import org.openjdk.jmh.annotations.Benchmark;
@@ -36,59 +38,87 @@ import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
-@Fork(value = 2, jvmArgsAppend = { "-server", "-XX:+UseG1GC" })
+@Fork(value = 2, jvmArgsAppend = {"-server", "-XX:+UseG1GC"})
 public class ScopeImplBenchmark {
 
     private static final DurationBuckets EXPONENTIAL_BUCKETS = DurationBuckets.linear(Duration.ofMillis(1), Duration.ofMillis(10), 128);
 
     private static final String[] COUNTER_NAMES = {
-        "first-counter",
-        "second-counter",
-        "third-counter",
-        "fourth-counter",
-        "fifth-counter",
+            "first-counter",
+            "second-counter",
+            "third-counter",
+            "fourth-counter",
+            "fifth-counter",
     };
 
     private static final String[] GAUGE_NAMES = {
-        "first-gauge",
-        "second-gauge",
-        "third-gauge",
-        "fourth-gauge",
-        "fifth-gauge",
+            "first-gauge",
+            "second-gauge",
+            "third-gauge",
+            "fourth-gauge",
+            "fifth-gauge",
     };
 
     private static final String[] HISTOGRAM_NAMES = {
-        "first-histogram",
-        "second-histogram",
-        "third-histogram",
-        "fourth-histogram",
-        "fifth-histogram",
+            "first-histogram",
+            "second-histogram",
+            "third-histogram",
+            "fourth-histogram",
+            "fifth-histogram",
     };
 
     @Benchmark
     public void scopeReportingBenchmark(BenchmarkState state) {
-        state.scope.reportLoopIteration();
+        state.reportingBenchmarkScope.reportLoopIteration();
+    }
+
+    @Benchmark
+    public void recordingWithSanitizingDashBenchmark(BenchmarkState state) {
+        state.recordTestMetrics(state.sanitizingBenchmarkScope);
+    }
+
+    @Benchmark
+    public void recordingBenchmark(BenchmarkState state) {
+        state.recordTestMetrics(state.recordingBenchmarkScope);
     }
 
     @State(org.openjdk.jmh.annotations.Scope.Benchmark)
     public static class BenchmarkState {
 
-        private ScopeImpl scope;
+        private ScopeImpl reportingBenchmarkScope;
+        private ScopeImpl sanitizingBenchmarkScope;
+        private ScopeImpl recordingBenchmarkScope;
 
         @Setup
         public void setup() {
-            this.scope =
-                    (ScopeImpl) new RootScopeBuilder()
-                            .reporter(new TestStatsReporter())
-                            .tags(
-                                    ImmutableMap.of(
-                                            "service", "some-service",
-                                            "application", "some-application",
-                                            "instance", "some-instance"
-                                    )
+            final ScopeBuilder scopeBuilder = new RootScopeBuilder()
+                    .reporter(new TestStatsReporter())
+                    .tags(
+                            ImmutableMap.of(
+                                    "service", "some-service",
+                                    "application", "some-application",
+                                    "instance", "some-instance"
                             )
-                            .reportEvery(Duration.MAX_VALUE);
+                    );
 
+            this.reportingBenchmarkScope =
+                    (ScopeImpl) scopeBuilder.reportEvery(Duration.MAX_VALUE);
+
+            this.recordTestMetrics(this.reportingBenchmarkScope);
+
+            this.recordingBenchmarkScope = (ScopeImpl) scopeBuilder.reportEvery(Duration.MAX_VALUE);
+            this.sanitizingBenchmarkScope =
+                    (ScopeImpl) scopeBuilder.sanitizer(
+                            new ScopeSanitizerBuilder()
+                                    .withNameValidCharacters(ValidCharacters.of(null, ValidCharacters.UNDERSCORE_CHARACTERS))
+                                    .withTagKeyValidCharacters(ValidCharacters.of(null, ValidCharacters.UNDERSCORE_CHARACTERS))
+                                    .withTagValueValidCharacters(ValidCharacters.of(null, ValidCharacters.UNDERSCORE_CHARACTERS))
+                                    .build()
+                    )
+                            .reportEvery(Duration.MAX_VALUE);
+        }
+
+        public void recordTestMetrics(final ScopeImpl scope) {
             for (String counterName : COUNTER_NAMES) {
                 scope.counter(counterName).inc(1);
             }
@@ -112,7 +142,7 @@ public class ScopeImplBenchmark {
 
         @TearDown
         public void teardown() {
-            scope.close();
+            reportingBenchmarkScope.close();
         }
 
     }
